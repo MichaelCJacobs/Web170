bba8f3aacd372e4451f119543a0c3387bec48975
MichaelCJacobs <mjacob23@seattlecentral.edu>
2017-04-14 14:10:42 -0700

    Beginning templates
    Initial Templates + a few new pictures of Sensei.

---
+++ SKA/index.php
@@ -0,0 +1,104 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+	
+<meta name="viewport" content="initial-scale=1">
+
+<title>Aikido with Dynamic Spirit | Seattle Kokikai Aikido | Central District & Capitol Hill</title>
+<meta name="description" content="Learn the modern martial-art Aikido in a safe and have fun at Seattle Kokikai Aikido Dojo, located in Seattle's Central District">
+
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script>
+		$(document).ready(function(){
+			$(".nav-button").click(function () {
+			$(".nav-button,.primary-nav").toggleClass("open");
+			});    
+		});
+		</script>
+
+<script type="text/javascript">
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+</script>
+	
+<!-- Flexslider CSS and JS -->
+<link rel="stylesheet" href="flex/css/flexslider.css" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="flex/js/jquery.flexslider.js"></script>
+
+<!-- Flexslider CSS and JS -->
+<link rel="stylesheet" href="flex/css/flexslider.css" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="flex/js/jquery.flexslider.js"></script>
+
+
+
+<!-- Load the flexslider -->
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+          animation: "fade",  // slide or fade
+          controlsContainer: ".flex-container" // the container that holds the flexslider
+    });
+  });
+</script>
+	
+	
+	
+		
+</head>
+
+<body>
+
+<?php include("includes/header.php"); ?>
+
+
+
+<div id="wrapper">
+
+<aside class="home">
+<img src="images/SenseiAtWInterCamp2008small.jpg" alt="Sensei Maruyama demonstrating">
+</aside>
+
+<main class="home">
+<h3>Aikido for Self-Defense, Personal Development and Fun</h3>
+<p>At Seattle Kokikai, we practice the modern martial art of Aikido in a safe and friendly environment. </p>
+<p>Our basic principles guide us towards minimum effort, maximum effect through proper use of timing, center, body-mechanics and our attacker’s own energy.  </p>
+<p>Aikido is an ethical and friendly martial art suitable for people of all ages and body types. 
+</p>
+</main>
+
+
+<!-- Slider Container and Images -->
+
+<div style="padding:5px;">
+	<div class="flexslider">
+ 		<ul class="slides">
+	 		<li><a href=""><img src="flex/images/slide1.jpg" /><p class="flex-caption"><h4>Calm</h4>Training to remain calm even when being physically threatened.</p></a></li>
+	 		<li><a href=""><img src="flex/images/slide2.jpg" /><p class="flex-caption"><h4>Awareness</h4> Personal Safety begins with situational awareness and practice being present in the moment.</p></a></li>
+	 		<li><a href=""><img src="flex/images/slide3.jpg" /><p class="flex-caption"><h4>Movement</h4> In Aikido we blend with the opponent’s movement to use their strength 	and momentum to achieve our goals </p></a></li>
+			<li><a href=""><img src="flex/images/slide4.jpg" /><p class="flex-caption"><h4>Technique</h4>Technique Minimum effort, for maximum effect.</p></a></li>
+	 	</ul>
+	 </div>
+</div>
+
+
+
+
+
+<blockquote>
+<img src = "images/dojo.png" alt="Our Dojo during testing 2015">
+
+</blockquote>
+</div> <!--  end wrapper  -->
+
+<?php include("includes/footer.php"); ?>
+
+</body>
+
+
+</html>
---
+++ SKA/learn-aikido.php
@@ -0,0 +1,93 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+	
+<meta name="viewport" content="initial-scale=1">
+
+<title>Aikido with Dynamic Spirit | Seattle Kokikai Aikido | Central District & Capitol Hill</title>
+<meta name="description" content="Learn the modern martial-art Aikido in a safe and have fun at Seattle Kokikai Aikido Dojo, located in Seattle's Central District">
+
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script>
+		$(document).ready(function(){
+			$(".nav-button").click(function () {
+			$(".nav-button,.primary-nav").toggleClass("open");
+			});    
+		});
+		</script>
+
+<script type="text/javascript">
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+</script>
+	
+<!-- Flexslider CSS and JS -->
+<link rel="stylesheet" href="flex/css/flexslider.css" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="flex/js/jquery.flexslider.js"></script>
+
+<!-- Flexslider CSS and JS -->
+<link rel="stylesheet" href="flex/css/flexslider.css" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="flex/js/jquery.flexslider.js"></script>
+
+
+
+<!-- Load the flexslider -->
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+          animation: "fade",  // slide or fade
+          controlsContainer: ".flex-container" // the container that holds the flexslider
+    });
+  });
+</script>
+	
+	
+	
+		
+</head>
+
+<body>
+
+<?php include("includes/header.php"); ?>
+
+
+
+<div id="wrapper">
+
+
+<main class="learn">
+<img src="images/weaponsoutside.jpg">
+<h1>How To Get Started</h1>
+<h3>To Try Aikido for yourself, just show up. </h3>
+</p>Arrive a few minutes early and introduce yourself.  Any of the students wearing colored belts will be happy to answer your questions and help you get started.  Wear workout clothes that you can move around in.   </p>
+
+<p>You’re also welcome to just come and watch, but experiencing practice for yourself is the best way to learn how you’ll enjoy the class.  </p>
+<p>
+Practice is free for members the <a href="http://www.seattleymca.org/Locations/MeredithMathews/pages/Home.aspx">YMCA</a>. If you’re interested, but are not a YMCA member, stop by the front desk and one of our members will sign you in as a guest.  
+</p>
+</main>
+
+
+
+
+
+
+
+<blockquote>
+<img src = "images/dylans-test.jpg" alt="Dylan-San's rank exam">
+
+</blockquote>
+</div> <!--  end wrapper  -->
+
+<?php include("includes/footer.php"); ?>
+
+</body>
+
+
+</html>
